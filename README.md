cccb199708068cc0da0534b74e4256b71f06e030
Tomasz Sterna <tomasz@sterna.link>
2023-03-10 08:24:34 +0100

    Add server skeleton

---
+++ README.md
@@ -0,0 +1,23 @@
+# My Little Game Engine
+
+## Preparation
+
+This project makes use of git submodules, so you need to `git clone --recursive`.
+
+## Building
+
+[`zig`][1] v. 0.11 required.
+
+    zig build
+
+[1]: https://ziglang.org
+
+## Running
+
+Client
+
+    zig build client
+
+Server
+
+    zig build server
--- build.zig
+++ build.zig
@@ -8,8 +8,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // ---- vendor ----
+
     const raylib = raylib_build.addRaylib(b, target, optimize);
     raylib.install();
+
+    // ---- shared game logic ----
 
     const shared = b.addStaticLibrary(.{
         .name = "shared",
@@ -29,57 +33,90 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
+    // ---- game graphical client ----
+
+    const client = b.addExecutable(.{
         .name = "client",
         .root_source_file = .{ .path = "client/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    exe.linkLibrary(shared);
+    client.linkLibrary(shared);
 
-    exe.addIncludePath("ext/raylib/src");
-    exe.linkLibrary(raylib);
+    client.addIncludePath("ext/raylib/src");
+    client.linkLibrary(raylib);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
-    exe.install();
+    client.install();
 
     // This *creates* a RunStep in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = exe.run();
+    const client_cmd = client.run();
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
     // This is not necessary, however, if the application depends on other installed
     // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+    client_cmd.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        client_cmd.addArgs(args);
     }
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const client_step = b.step("client", "Run the client");
+    client_step.dependOn(&client_cmd.step);
 
     // Creates a step for unit testing.
-    const exe_tests = b.addTest(.{
+    const client_tests = b.addTest(.{
         .root_source_file = .{ .path = "client/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
+    // ---- headless game server ----
+
+    const server = b.addExecutable(.{
+        .name = "server",
+        .root_source_file = .{ .path = "server/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    server.linkLibrary(shared);
+
+    server.install();
+
+    const server_cmd = server.run();
+    server_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        server_cmd.addArgs(args);
+    }
+
+    const server_step = b.step("server", "Run the server");
+    server_step.dependOn(&server_cmd.step);
+
+    const server_tests = b.addTest(.{
+        .root_source_file = .{ .path = "server/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // ---- unit testing ----
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&shared_tests.step);
-    test_step.dependOn(&exe_tests.step);
+    test_step.dependOn(&client_tests.step);
+    test_step.dependOn(&server_tests.step);
 }
